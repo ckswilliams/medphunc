ee55432d5b5a975e0d9118d171b16f5b69012a4f
WILLIAMS <WILLCX@austin.org.au>
2021-12-03 15:41:27 +1100

    add basic functionality for NM shielding

--- medphunc/misc/Archer.py
+++ medphunc/misc/Archer.py
@@ -17,15 +17,22 @@ class Archer:
     def __init__(self):
         self.df_att_coeff = pd.read_csv(att_coeff_fn)
         
+    @staticmethod
+    def calculate_transmission(a, b, y, thickness):
+        return ((1 + b/a) * np.exp(a*y*thickness) - b/a)**(-1/y)
+    
+    @staticmethod
+    def calculate_shielding(a, b, y, transmission):
+        return 1 / (a*y) * np.log((transmission**(-y) + (b/a)) / (1+(b/a)))
+    
     #Transmission calculation functions
     def shielding_to_transmission(self, thickness, material, kV):
         a, b, y = self.get_shielding_coefficients(material, kV)
-        return ((1 + b/a) * np.exp(a*y*thickness) - b/a)**(-1/y)
+        return self.calculate_transmission(a, b, y, thickness)
 
     def transmission_to_shielding(self, transmission, material, kV):
         a, b, y = self.get_shielding_coefficients(material, kV)
-        x = 1 / (a*y) * np.log((transmission**(-y) + (b/a)) / (1+(b/a)))
-        return x
+        return self.calculate_shielding(a, b, y, transmission)
 
     def get_shielding_coefficients(self, material, kV):
         view = self.df_att_coeff.loc[self.df_att_coeff.Material == material,
@@ -38,15 +45,20 @@ class Archer:
         y = np.interp(kV, xp, yp[:, 2])
         return a, b, y
 
-
 #%%
 
-class Kusano:
+class Kusano(Archer):
+    "Superceded by NM, which includes other isotopes"
+    att_coeff_fn = os.path.split(__file__)[0]+'/nm_input_shielding_coefficients.csv'
+    
+    
     def __init__(self):
         self.df_att_coeff = pd.DataFrame([['I-131','Lead',0.1078,0.2003,0.4957],
                                             ['I-131','Concrete', 0.1705, -0.1308, 1.038]],
                                          columns=['Isotope','Material','a','b','y'])
-        
+    
+    
+    
     #Transmission calculation functions
     def shielding_to_transmission(self, thickness, material, isotope):
         a, b, y = self.get_shielding_coefficients(material, isotope)
@@ -61,19 +73,71 @@ class Kusano:
         view = self.df_att_coeff.query("Isotope==@isotope").query('Material==@material')
         return view.loc[:,['a','b','y']].iloc[0,:]
     
+    
+    
+    
 k = Kusano()
 k.shielding_to_transmission(15.4, 'Lead','I-131')
 
-#%%    
-    
-        #view.index = view.kV
-        xp = np.array(view['Isotope'])
-        yp = np.array(view[['a', 'b', 'y']])
-        a = np.interp(isotope, xp, yp[:, 0])
-        b = np.interp(isotope, xp, yp[:, 1])
-        y = np.interp(isotope, xp, yp[:, 2])
-        return a, b, y
 
+
+#%%
+
+class NM(Archer):
+    #att_coeff_fn = os.path.split(__file__)[0]+'/nm_input_shielding_coefficients.csv'
+    att_coeff_fn = 'nm_input_shielding_coefficients.csv'
+    
+    def __init__(self):
+        self.att_coeff = pd.read_csv(self.att_coeff_fn)
+        self.att_coeff.value = pd.to_numeric(self.att_coeff.value, errors='coerce')
+    
+    
+    def show_options(self):
+        print('Radionuclides:')
+        print(self.att_coeff.radionuclide.unique())
+        print('Materials:')
+        print(self.att_coeff.query("fit_method=='Archer'").material.unique())
+        print('Only Archer fits have been implemented')
+    
+    def get_shielding_coefficients(self, radionuclide, material, fit_method='Archer'):
+        
+        df = self.att_coeff
+        names = ['radionuclide', 'fit_method', 'material']
+        values = [radionuclide, fit_method, material]
+        
+        for value, name in zip(values, names):
+            df = df.loc[df[name]==value,:]
+            if df.shape[0] == 0:
+                raise(ValueError(f'Invalid combination of inputs: failed at finding {value} in {name}'))
+        
+        if all(df.value.isna()):
+            raise(ValueError(f'Invalid combination of inputs: no published value for {radionuclide} using {fit_method} in {material}'))
+
+        return {k:v for k, v in zip(df.parameter_name, df.value)}
+    
+    
+    #Transmission calculation functions
+    def shielding_to_transmission(self, radionuclide, material, thickness):
+        coeff = self.get_shielding_coefficients(radionuclide, material, fit_method='Archer')
+        a, b, y = coeff['alpha'], coeff['beta'], coeff['gamma']
+        return self.calculate_shielding(a, b, y, thickness)
+
+    def transmission_to_shielding(self, radionuclide, material, transmission):
+        coeff = self.get_shielding_coefficients(radionuclide, material, fit_method='Archer')
+        a, b, y = coeff['alpha'], coeff['beta'], coeff['gamma']
+        return self.calculate_shielding(a, b, y, transmission)
+    
+#    def nth_vl_best_match(self, radionuclide, material):
+#        isotope_coeffs = self.get_shielding_coefficients(radionuclide, 'Nth value layer', material)
+#        over_values = self.att_coeff.query("fit_method=='Nth value layer'").query("material==@material")
+
+
+
+class Groth(NM):
+    pass
+    
+    
+   
 
 #%% Testing
 if __name__=='__main__':
